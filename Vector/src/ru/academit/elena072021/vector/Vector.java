60dad14a4e5a12630ef39defba9f133c4352b523
Elena-2021 <hape2@rambler.ru>
2021-08-21 19:27:24 +0700

    Выполнены исправления в задаче "Вектор".

--- Vector/src/ru/academit/elena072021/vector/Vector.java
+++ Vector/src/ru/academit/elena072021/vector/Vector.java
@@ -20,10 +20,6 @@ public class Vector {
             throw new NullPointerException("The argument must not be = null");
         }
 
-        if (vector.coordinates.length == 0) {
-            throw new IllegalArgumentException("Length of Vector must not be = 0");
-        }
-
         coordinates = Arrays.copyOf(vector.coordinates, vector.coordinates.length);
     }
 
@@ -50,7 +46,7 @@ public class Vector {
             throw new NullPointerException("The argument must not be = null");
         }
 
-        coordinates = Arrays.copyOf(array, Math.min(size, array.length));
+        coordinates = Arrays.copyOf(array, size);
     }
 
     // Метод getSize() для получения размерности вектора
@@ -61,25 +57,22 @@ public class Vector {
     // Метод toString(), чтобы выдавал информацию о векторе в формате { значения компонент через запятую }
     @Override
     public String toString() {
-        StringBuilder temp = new StringBuilder();
-        temp.append("{");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
 
-        for (int i = 0; i < coordinates.length; i++) {
-
-            if (i != 0) {
-                temp.append(", ");
-            }
-
-            temp.append(coordinates[i]);
+        for (double coordinate : coordinates) {
+            stringBuilder.append(", ");
+            stringBuilder.append(coordinate);
         }
 
-        temp.append("}");
+        stringBuilder.append("}");
+        stringBuilder.delete(1, 3);
 
-        return temp.toString();
+        return stringBuilder.toString();
     }
 
     // a. Прибавление к вектору другого вектора
-    public void addition(Vector vector) {
+    public void addUp(Vector vector) {
         if (vector == null) {
             throw new NullPointerException("The argument must not be = null");
         }
@@ -88,15 +81,13 @@ public class Vector {
             coordinates = Arrays.copyOf(coordinates, vector.coordinates.length);
         }
 
-        int size = Math.min(coordinates.length, vector.coordinates.length);
-
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < vector.coordinates.length; i++) {
             coordinates[i] += vector.coordinates[i];
         }
     }
 
     // b. Вычитание из вектора другого вектора
-    public void subtraction(Vector vector) {
+    public void subtract(Vector vector) {
         if (vector == null) {
             throw new NullPointerException("The argument must not be = null");
         }
@@ -105,15 +96,13 @@ public class Vector {
             coordinates = Arrays.copyOf(coordinates, vector.coordinates.length);
         }
 
-        int size = Math.min(coordinates.length, vector.coordinates.length);
-
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < vector.coordinates.length; i++) {
             coordinates[i] -= vector.coordinates[i];
         }
     }
 
     // c. Умножение вектора на скаляр
-    public void multiplicationByScalar(double multiplier) {
+    public void multiplyByScalar(double multiplier) {
         for (int i = 0; i < coordinates.length; i++) {
             coordinates[i] *= multiplier;
         }
@@ -121,41 +110,41 @@ public class Vector {
 
     // d. Разворот вектора (умножение всех компонент на -1)
     public void reversal() {
-        multiplicationByScalar(-1);
+        multiplyByScalar(-1);
     }
 
     // e. Получение длины вектора
     public double getLength() {
-        double temp = 0;
+        double coordinatesSquaresSum = 0;
 
         for (double v : coordinates) {
-            temp += v * v;
+            coordinatesSquaresSum += v * v;
         }
 
-        return Math.sqrt(temp);
+        return Math.sqrt(coordinatesSquaresSum);
     }
 
     // f. Получение компоненты вектора по индексу
     public double getComponent(int index) {
         if (index < 0) {
-            throw new IllegalArgumentException("Index (" + index + ") must be >= 0");
+            throw new ArrayIndexOutOfBoundsException("Index (" + index + ") must be >= 0");
         }
 
         if (index >= coordinates.length) {
-            throw new IllegalArgumentException("Index (" + index + ") must be <= length of vector");
+            throw new ArrayIndexOutOfBoundsException("The index (" + index + ") must be <= than the length of the vector");
         }
 
         return coordinates[index];
     }
 
     // f. Установка компоненты вектора по индексу
-    public void setComponent(double component, int index) {
+    public void setComponent(int index, double component) {
         if (index < 0) {
-            throw new IllegalArgumentException("Index (" + index + ") must be >= 0");
+            throw new ArrayIndexOutOfBoundsException("Index (" + index + ") must be >= 0");
         }
 
         if (index >= coordinates.length) {
-            throw new IllegalArgumentException("Index (" + index + ") must be <= length of vector");
+            throw new ArrayIndexOutOfBoundsException("The index (" + index + ") must be less than or equal to the length of the vector");
         }
 
         coordinates[index] = component;
@@ -165,19 +154,25 @@ public class Vector {
     @Override
     public boolean equals(Object o) {
         if (o == null) {
-            throw new NullPointerException("The argument shouldn't be = null");
-        }
-
-        if (this.getClass() != o.getClass()) {
-            throw new IllegalArgumentException("The argument is not an instance of the Vector class");
-        }
-
-        if (coordinates.length != ((Vector) o).coordinates.length) {
             return false;
         }
 
-        for (double coordinate : coordinates) {
-            if (coordinate != ((Vector) o).coordinates.length) {
+        if (this == o) {
+            return true;
+        }
+
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+
+        Vector objectVector = (Vector) o;
+
+        if (coordinates.length != objectVector.coordinates.length) {
+            return false;
+        }
+
+        for (int i = 0; i < coordinates.length; i++) {
+            if (coordinates[i] != objectVector.coordinates[i]) {
                 return false;
             }
         }
@@ -188,12 +183,11 @@ public class Vector {
     // g. Соответственно, переопределить hashCode
     @Override
     public int hashCode() {
-
         return Arrays.hashCode(coordinates);
     }
 
     // a. Сложение двух векторов – должен создаваться новый вектор
-    public static Vector getAddition(Vector vector1, Vector vector2) {
+    public static Vector getAmount(Vector vector1, Vector vector2) {
         if (vector1 == null) {
             throw new NullPointerException("First argument shouldn't be = null");
         }
@@ -202,17 +196,14 @@ public class Vector {
             throw new NullPointerException("Second argument shouldn't be = null");
         }
 
-        int size = Math.max(vector1.coordinates.length, vector2.coordinates.length);
-        Vector vector = new Vector(size);
-
-        vector.addition(vector1);
-        vector.addition(vector2);
+        Vector vector = new Vector(vector1);
+        vector.addUp(vector2);
 
         return vector;
     }
 
     // b. Вычитание векторов – должен создаваться новый вектор
-    public static Vector getSubtraction(Vector vector1, Vector vector2) {
+    public static Vector getDifference(Vector vector1, Vector vector2) {
         if (vector1 == null) {
             throw new NullPointerException("First argument shouldn't be = null");
         }
@@ -221,11 +212,8 @@ public class Vector {
             throw new NullPointerException("Second argument shouldn't be = null");
         }
 
-        int size = Math.max(vector1.coordinates.length, vector2.coordinates.length);
-        Vector vector = new Vector(size);
-
-        vector.addition(vector1);
-        vector.subtraction(vector2);
+        Vector vector = new Vector(vector1);
+        vector.subtract(vector2);
 
         return vector;
     }
@@ -241,12 +229,12 @@ public class Vector {
         }
 
         int size = Math.min(vector1.coordinates.length, vector2.coordinates.length);
-        double temp = 0;
+        double currentScalarProduct = 0;
 
         for (int i = 0; i < size; i++) {
-            temp += vector1.coordinates[i] * vector2.coordinates[i];
+            currentScalarProduct += vector1.coordinates[i] * vector2.coordinates[i];
         }
 
-        return temp;
+        return currentScalarProduct;
     }
 }
--- Vector/src/ru/academit/elena072021/vector_main/Main.java
+++ Vector/src/ru/academit/elena072021/vector_main/Main.java
@@ -17,13 +17,13 @@ public class Main {
             Vector vector4 = new Vector(15, new double[]{10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0});
             System.out.println("vector4 = " + vector4);
 
-            Vector vector5 = Vector.getAddition(vector2, vector4);
+            Vector vector5 = Vector.getAmount(vector2, vector4);
             System.out.println("vector5 = " + vector5);
 
-            Vector vector6 = Vector.getSubtraction(vector2, vector4);
+            Vector vector6 = Vector.getDifference(vector2, vector4);
             System.out.println("vector6 = " + vector6);
 
-            System.out.println("Проверка на равенство vector1 и vector2 = " + vector1.equals(vector2));
+            System.out.println("Проверка на равенство vector2 и vector3 = " + vector2.equals(vector3));
 
             System.out.println("Сравнение hashCode у vector2 и vector3: " + vector2.hashCode() + " и " + vector3.hashCode());
 
@@ -31,7 +31,7 @@ public class Main {
 
             System.out.println("Скалярное произведение векторов: vector5 и vector6 = " + Vector.getScalarProduct(vector5, vector6));
 
-            vector2.multiplicationByScalar(3);
+            vector2.multiplyByScalar(3);
             System.out.println("vector2 = " + vector2);
 
             Vector vector7 = null;
@@ -40,7 +40,7 @@ public class Main {
             Vector vector9 = new Vector(new double[]{1.0, 1.0, 1.0, 1.0});
             System.out.println("vector9 = " + vector9);
 
-        } catch (IllegalArgumentException | NullPointerException e1) {
+        } catch (IllegalArgumentException | NullPointerException | ArrayIndexOutOfBoundsException e1) {
             //System.out.println();
             e1.printStackTrace();
         }
@@ -51,16 +51,16 @@ public class Main {
         Vector vector12 = new Vector(new double[]{0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0});
         System.out.println("vector12 = " + vector12);
 
-        vector12.addition(vector11);
+        vector12.addUp(vector11);
         System.out.println("vector12 = " + vector12);
 
         Vector vector13 = new Vector(new double[]{0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0});
-        vector13.subtraction(vector12);
+        vector13.subtract(vector12);
         System.out.println("vector13 = " + vector13);
 
         System.out.println("Длина vector13 = " + vector13.getLength());
         System.out.println("12 компанент vector13 = " + vector13.getComponent(11));
-        vector13.setComponent(333.0, 10);
+        vector13.setComponent(10,333.0);
         System.out.println("vector13 = " + vector13);
         System.out.println("Размерность vector13 = " + vector13.getSize());
     }
